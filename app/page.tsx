ed29c0db55ce87ed02b2b7af168080c9ba76a49f
vikram-parashar <vikramparashar24@gmail.com>
2025-08-05 01:17:10 +0530

    latest package installed

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,42 @@
+"use client"
 import Footer from "@/components/Footer";
 import Hero from "@/components/home/Hero";
 import Categories from "@/components/home/Categories";
 import OurCustomers from "@/components/home/Customers";
 import Canvas from "@/components/home/Canvas";
 import { getCategories, getCustomers } from "@/lib/queries";
+import { Button } from "@/components/ui/button"
+import { toast } from "sonner"
 
-export const revalidate = 3600;
-export default async function Home() {
-  const categories = await getCategories();
-  const customers = await getCustomers();
+
+// export const revalidate = 3600;
+export default function Home() {
+  // const categories = await getCategories();
+  // const customers = await getCustomers();
 
   return (
-    <div className="bg-transparent w-screen overflow-hidden relative">
+    <div>
+      <Button
+        onClick={() =>
+          toast("Event has been created", {
+            description: "Sunday, December 03, 2023 at 9:00 AM",
+            action: {
+              label: "Undo",
+              onClick: () => console.log("Undo"),
+            },
+          })
+        }
+      >
+        Show Toast
+      </Button>
+
+      {/*
       <Canvas />
       <Hero />
       <Categories categories={categories} />
       <OurCustomers customers={customers} />
       <Footer />
-    </div>
+      */}
+    </div >
   );
 }
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,17 +1,6 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
-import { Database } from "@/lib/types"
-type CartItemType = Database['public']['Tables']['users']['Row']['cart']
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
-
-export const addPrice = (cart: CartItemType[]) => {
-  return cart.reduce((total: number, item) => {
-    const price: number = item?.['product'].price ?? 0;
-    const quantity: number = item?.['quantity'] ?? 0;
-    return total + price * quantity;
-  }, 0);
-};
-
